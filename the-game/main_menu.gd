09056fa6e9277015eeec371a5127c540b17dd3ef
mohkhl23-prog <mohkhl23@kingshigh.school.nz>
2026-08-26 12:53:38 +1200

    1.12 Final verion

--- the-game/main_menu.gd
+++ the-game/main_menu.gd
@@ -12,12 +12,12 @@ func _process(delta: float) -> void:
 
 
 func _on_start_pressed() -> void:
-	pass # Replace with function body.
+	get_tree().change_scene_to_file("res://racetrack.tscn")
 
 
 func _on_settings_pressed() -> void:
-	pass # Replace with function body.
+	print("Setting pressed ")
 
 
 func _on_exit_pressed() -> void:
-	pass # Replace with function body.
+	get_tree().quit()
